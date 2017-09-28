b6716ecf6ff17196e4fc904dff0f5395e04f432d
jakaria08 <jakaria.rabbi@live.com>
2017-09-27 21:43:30 -0600

    LAB 4

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -1,3 +1,14 @@
+/*
+ * Class Name : Tweet
+ *
+ * Version: Version 1.0
+ *
+ * Date : September 28, 2017
+ *
+ * Copyright (c) 2017 Team X , CMPUT301, University of Alberta - All Rights Reserved. You may use, distribute, or modify this code under terms and conditions of the Code of Student Behavior at University of Alberta.
+ * You can find a copy of the license in this project. Otherwise please contact contact@ualberta.ca
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.io.BufferedReader;
